31d1afcce1e07d3009e7a6a83f23a71d0def64da
AngelVargas1980 <49385201+AngelVargas1980@users.noreply.github.com>
2021-07-25 14:47:49 -0600

    primer commit

---
+++ src/main.java
@@ -0,0 +1,13 @@
+public class main {
+    public static void main(String[] args){
+        System.out.println("Hola Mundo");
+        int num = 5;
+
+        int num2 = 5;
+
+        int suma = num + num2;
+
+        System.out.println(suma);
+
+    }
+}
